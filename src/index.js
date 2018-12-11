3f41a90d5da9d630cd348d70ed5514bf2f143ea3
Tim Toohey <ttoohey@users.noreply.github.com>
2018-12-11 19:50:26 +1100

    add ability to debounce (delay) display of validation messages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { createValidator, getPayload, ValidatorError } from "validator-creator";
 export * from "validator-creator";
 
 const initialState = {};
 const initialChanges = {};
+const defaultOptions = {
+  delay: 0
+};
 
 function merge(...args) {
   let result = {};
@@ -37,17 +40,37 @@ export function ignoreValidatorError(error) {
   }
 }
 
-export default function(rules, rules1 = {}) {
+export default function(rules, rules1 = {}, options = {}) {
+  const opt = { ...defaultOptions, ...options };
   const [state0, setState0] = useState(initialState);
   const [state1, setState1] = useState(initialState);
   const [, setChanges] = useState(initialChanges);
   const validate0 = useMemo(() => createValidator(rules), []);
   const validate1 = useMemo(() => createValidator(rules1), []);
   const messages = useMemo(() => merge(state0, state1), [state0, state1]);
+  const [interval, setInterval] = useState(0);
+  const [delayedMessages, setDelayedMessages] = useState({});
+  useEffect(
+    () => {
+      if (interval < 0) {
+        return;
+      }
+      const timeout = setTimeout(() => setDelayedMessages(messages), interval);
+      return () => clearTimeout(timeout);
+    },
+    [messages, interval]
+  );
   const validate = async (change = null, ...args) => {
+    const start = Date.now();
+    setInterval(-1);
     const changes = await update(setChanges, change);
     const payload0 = await validate0(change, ...args).then(getPayload);
     const payload1 = await validate1(changes, ...args).then(getPayload);
+    setInterval(
+      change === null || args.length > 1
+        ? 0
+        : Math.max(0, opt.delay - (Date.now() - start))
+    );
     return merge(
       ...(await Promise.all([
         update(setState0, payload0),
@@ -60,5 +83,5 @@ export default function(rules, rules1 = {}) {
     setState1(initialState);
     setChanges(initialChanges);
   };
-  return [messages, validate];
+  return [delayedMessages, validate];
 }
